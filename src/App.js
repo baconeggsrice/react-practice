22716683b923d86d1ddbc8ad58e7af16dba772a9
Garrick Wong <inquisitor8986@gmail.com>
2024-10-19 01:08:14 -0600

    More files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import './App.css';
-
-const About = () => {
-  return (
-    <div className="About">
-      <>
-        <h1>About Me</h1>
-        <h2>I'm a software engineering student at the University of Alberta.</h2>
-      </>
-      
-    </div>
-  )
-}
+import Introduction from "./components/Introduction";
+import Projects from "./components/Projects";
 
 const App = () => {
   return (
     <div className="App">
-      <About />
+      <Introduction />
+      <Projects />
     </div>
   );
 }
---
+++ src/components/Projects.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Projects = () => {
+    return (
+        <>
+            <h2>Programs I've coded:</h2>
+            <h3>Conway's Game of Life</h3>
+        </>
+    );
+}
+
+export default Projects;
