81af9351c96dcc4ff1cccadae9f00183b9e296ee
Garrick Wong <inquisitor8986@gmail.com>
2024-10-18 23:12:21 -0600

    Add files.

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+const About = () => {
+  return (
+    <div className="About">
+      <>
+        <h1>About Me</h1>
+        <h2>I'm a software engineering student at the University of Alberta.</h2>
+      </>
+      
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div className="App">
+      <About />
+    </div>
+  );
+}
+
+export default App;
+
